d3359d4c14594f2d4e91b5b53cac8834a2754573
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-28 14:15:20 -0300

    programa para apresentar uma mensagem de apresentação com dados

---
+++ 03/index.js
@@ -0,0 +1,52 @@
+const usuario = {
+    nome: "João",
+    idade: 25,
+    profissao: "programador",
+    cor_preferida: "verde"
+};
+
+const { nome, idade, profissao, cor_preferida } = usuario;
+
+const mensagem = `Olá.. Eu sou ${nome} e tenho ${idade} anos de idade. Sou ${profissao} e minha cor preferida é ${cor_preferida}!`;
+
+console.log(mensagem);
+
+/* Imprime
+Olá.. Eu sou João e tenho 25 anos de idade. Sou programador e minha cor preferida é verde!
+*/
+
+// Outros exemplos
+
+const usuario = {
+    nome: "Leticia",
+    idade: 26,
+    profissao: "biomédica",
+    cor_preferida: "roxo"
+};
+
+const { nome, idade, profissao, cor_preferida } = usuario;
+
+const mensagem = `Olá.. Eu sou ${nome} e tenho ${idade} anos de idade. Sou ${profissao} e minha cor preferida é ${cor_preferida}!`;
+
+console.log(mensagem);
+
+/* Imprime
+Olá.. Eu sou Leticia e tenho 26 anos de idade. Sou biomédica e minha cor preferida é roxo!
+*/
+
+const usuario = {
+    nome: "Lúcia",
+    idade: 45,
+    profissao: "professora",
+    cor_preferida: "rosa"
+};
+
+const { nome, idade, profissao, cor_preferida } = usuario;
+
+const mensagem = `Olá.. Eu sou ${nome} e tenho ${idade} anos de idade. Sou ${profissao} e minha cor preferida é ${cor_preferida}!`;
+
+console.log(mensagem);
+
+/* Imprime
+Olá.. Eu sou Lúcia e tenho 45 anos de idade. Sou professora e minha cor preferida é rosa!
+*/ 
